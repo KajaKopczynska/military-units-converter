fb242beeabac173160067cff6d42179053289158
KajaKopczynska <kaja.a.kopczynska@gmail.com>
2023-05-17 11:23:04 +0200

    styled.js changed

--- src/App/Clock/index.js
+++ src/App/Clock/index.js
@@ -1,4 +1,3 @@
-import React, { useState, useEffect } from "react";
 import { useCurrentDate } from "./useCurrentDate";
 import { StyledClock } from "./styled";
 
--- src/App/Form/styled.js
+++ src/App/Form/styled.js
@@ -4,7 +4,7 @@ export const StyledForm = styled.form`
     margin: 8px;
     background-color: rgb(245, 249, 255);
     padding: 5px;
-    max-width: 330px;
+    width: 400px;
     height: 455px;
     background: #D5C9AF;
     box-shadow: 10px 10px 49px #000000,
@@ -12,13 +12,21 @@ export const StyledForm = styled.form`
     border: 5px solid brown;
     border-image: url(https://dspl.innogamescdn.com/asset/c1748d3c/graphic/start2/border-input.jpg) 3 repeat;
     background: url(https://dspl.innogamescdn.com/asset/c1748d3c/graphic/start2/background.jpg) #c9b997;
+
+    @media (max-width: 767px) {
+        width: 330px;
+    }
     
 `;
 
 export const Header = styled.h1`
-    font-size: 22px;
+    font-size: 24px;
     font-weight: 500;
     line-height: 1.4;
+
+    @media (max-width: 767px) {
+        font-size: 24px;
+    }
 `;
 
 export const Unit = styled.select`
@@ -31,12 +39,16 @@ export const Unit = styled.select`
 `;
 
 export const Amount = styled.input`
-    width: 110px;
+    width: 180px;
     height: 30px;
     background: #e1cfb2;
     padding-left: 15px;
     border: 3px solid brown;
     border-image: url(https://dspl.innogamescdn.com/asset/c1748d3c/graphic/start2/border-input.jpg) 3 repeat;
+
+    @media (max-width: 767px) {
+    width: 110px;
+    }
 `;
 
 export const Button = styled.button`
@@ -45,13 +57,17 @@ export const Button = styled.button`
     margin: 10px;
     font-weight: bold;
     color: #222;
-    width: 270px;
+    width: 310px;
     background: #8D7C58;
     box-shadow: 2px 2px 5px #616161,
         -2px -2px 5px #616161;
     transition: 0.3s;
     border: 3px solid brown;
     border-image: url(https://dspl.innogamescdn.com/asset/c1748d3c/graphic/start2/border-input.jpg) 3 repeat;
+
+    @media (max-width: 767px) {
+    width: 270px;
+    }
 
     &:hover {
     color: #540909;
@@ -62,7 +78,7 @@ export const Button = styled.button`
 export const StyledResult = styled.div`
     margin: 0 auto;
     padding: 5px;
-    width: 270px;
+    width: 310px;
     font-weight: 400;
     line-height: 1.3;
     color: #222;
@@ -70,4 +86,8 @@ export const StyledResult = styled.div`
     text-align: left;
     border: 3px solid brown;
     border-image: url(https://dspl.innogamescdn.com/asset/c1748d3c/graphic/start2/border-input.jpg) 3 repeat;
+
+    @media (max-width: 767px) {
+    width: 270px;
+    }
 `;
